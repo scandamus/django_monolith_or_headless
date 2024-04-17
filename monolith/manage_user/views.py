048c8a515d009cd3a04738e664b6ff588193fa6e
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-17 15:43:39 +0900

    :dizzy:[update]templeteではなくHttpResponseで表示するパターン

--- monolith/manage_user/views.py
+++ monolith/manage_user/views.py
@@ -1,5 +1,5 @@
-from django.views import generic
+from django.http import HttpResponse
 
 
-class IndexView(generic.TemplateView):
-    template_name = "index.html"
+def index(request):
+    return HttpResponse("Hello, world.")
