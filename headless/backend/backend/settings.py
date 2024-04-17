4c43fc50a8da96d582d6e41869fad2bc5f2c9a77
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-17 20:52:20 +0900

    :sparkles:[add]headless userリストのapi end point追加

--- headless/backend/backend/settings.py
+++ headless/backend/backend/settings.py
@@ -68,6 +68,13 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
--- headless/backend/manage_user/views.py
+++ headless/backend/manage_user/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, renderers
 from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    renderer_classes = [renderers.JSONRenderer]
