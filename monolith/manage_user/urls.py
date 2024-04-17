24036d38dc87b769d00c7597192d68aef61330f3
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-17 16:37:01 +0900

    :sparkles:[add]user list, templete base

--- monolith/manage_user/urls.py
+++ monolith/manage_user/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path('users/', views.UserListView.as_view(), name='list_user'),
 ]
--- monolith/manage_user/views.py
+++ monolith/manage_user/views.py
@@ -1,5 +1,13 @@
 from django.views import generic
+from django.contrib.auth.models import User
+from django.views.generic import ListView
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'list_user.html'
+    context_object_name = 'users'
