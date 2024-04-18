8fcc6a37261a4dd68a04cab264106dc9ba12af36
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-19 02:54:27 +0900

    :dizzy:[update].env共通化

--- monolith/monolith/settings.py
+++ monolith/monolith/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv('./../.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
