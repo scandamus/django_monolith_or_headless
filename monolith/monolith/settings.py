ea7e76ab1f172681d2198dcc42fdf880f67d6efc
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-19 01:35:18 +0900

    :dizzy:[update]sqllite => postgresql

--- monolith/monolith/settings.py
+++ monolith/monolith/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -78,8 +82,12 @@ WSGI_APPLICATION = 'monolith.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': os.environ.get('SUPABASE_USR'),
+        'PASSWORD': os.environ.get('SUPABASE_PW'),
+        'HOST': os.environ.get('SUPABASE_HOST'),
+        'PORT': 5432
     }
 }
 
